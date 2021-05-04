16f785aa4d6cffce4689ad6c9c919731920e127d
Joshua Solomon <software@jsxshq.com>
2021-05-03 18:21:42 -0700

    Implements HTML structure of the Form Component.

--- src/landing-page/form-section/form/index.jsx
+++ src/landing-page/form-section/form/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./index.css";
 
-const HEADER_TEXT = "Subscribe Now"
-const TAGLINE_TEXT = "Help us solve fusion :) Or something smaller like a good hot sauce recipe.."
-
 const Form = props => {
 
-    return <form></form>
+    return <form>
+        <label htmlFor={"email"}>Email address</label>
+        <input type={"email"} name={"email"} />
+        <input type={"checkbox"} name={"terms_and_conditions"} value={"terms_and_conditions"} />
+        <label htmlFor={"terms_and_conditions"}>I agree to the terms and conditions</label>
+        <input type={"submit"} value={"Sign Up"} />
+    </form>
 }
 
 export default Form;
