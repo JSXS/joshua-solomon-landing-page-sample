0404718d8e8a4e87e3dab9a6c009b1b354b11e1b
Joshua Solomon <software@jsxshq.com>
2021-05-03 21:47:47 -0700

    Implements saga to post form data to the API.

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,13 @@
 export const ActionTypes = {
     "CLICK_SIGN_UP_BUTTON": "CLICK_SIGN_UP_BUTTON",
+    "DISPLAY_SUCCESS": "DISPLAY_SUCCESS",
     "SET_EMAIL": "SET_EMAIL",
     "TOGGLE_TERMS_AND_CONDITIONS": "TOGGLE_TERMS_AND_CONDITIONS"
 }
 
-export const clickSignUpButton = (value) => ({ type: ActionTypes.CLICK_SIGN_UP_BUTTON });
+export const clickSignUpButton = () => ({ type: ActionTypes.CLICK_SIGN_UP_BUTTON });
+
+export const displaySuccess = () => ({ type: ActionTypes.DISPLAY_SUCCESS });
 
 export const setEmail = (value) => ({ type: ActionTypes.SET_EMAIL, value });
 
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,7 +1,9 @@
 import "core-js";
-import { debounce } from "redux-saga/effects";
+import fetch from 'cross-fetch';
+import { call, debounce, put, select } from "redux-saga/effects";
 import "regenerator-runtime/runtime";
-import { ActionTypes } from "./actions";
+import { ActionTypes, displaySuccess } from "./actions";
+import { stateSelector } from "./selectors";
 
 const DEBOUNCE_MS = 500;
 
@@ -14,8 +16,19 @@ function* clickSignUpButtonWatcher() {
     yield debounce(DEBOUNCE_MS, ActionTypes.CLICK_SIGN_UP_BUTTON, clickSignUpButtonWorker);
 }
 
-function* clickSignUpButtonWorker(initializeRootAction) {
+function* clickSignUpButtonWorker() {
 
-    console.log("CLICK WORKER");
+    const state = yield select(stateSelector);
+    const body = JSON.stringify(state);
 
+    const response = yield call(fetch, "https://defero.dev/api/test/josh", { method: "post", headers: { "Content-Type": "application/json" }, body });
+
+    if (response?.status === 200) {
+
+        yield put(displaySuccess());
+
+    } else {
+
+        console.warn(response);
+    }
 }
